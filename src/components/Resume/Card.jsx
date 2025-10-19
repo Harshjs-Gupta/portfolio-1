cc2c784a52f3e0390da8937ec3cef39850a7f5d6
HarshGupta <harshgupta88911@gmail.com>
2025-10-19 14:04:12 +0530

    update: stying on card

--- src/components/Resume/Card.jsx
+++ src/components/Resume/Card.jsx
@@ -1,21 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Card = (props) => {
+const Card = ({ title, subtitle, date, description, category }) => {
   const [showInfo, setShowInfo] = useState(false);
 
   return (
-    <div className='resume__item border-b-[2px] border-b-solid border-b-borderColor dark:border-b-borderColorDark '>
-      <div className='resume__header' onClick={() => setShowInfo(!showInfo)}>
-        <h3 className='resume__subtitle text-titleColor dark:text-titleColorDark'>{props.title}</h3>
-        <span className='resume__icon dark:bg-titleColorDark dark:text-bgColorDark bg-titleColor text-bgColorAlt'>{showInfo ? '-' : '+'}</span>
+    <div className="resume__item border-b-[2px] border-b-solid border-b-borderColor dark:border-b-borderColorDark ">
+      <div className="resume__header" onClick={() => setShowInfo(!showInfo)}>
+        <h3 className="resume__subtitle text-titleColor dark:text-titleColorDark">
+          {title}
+        </h3>
+        <span className="resume__icon dark:bg-titleColorDark dark:text-bgColorDark bg-titleColor text-bgColorAlt">
+          {showInfo ? "-" : "+"}
+        </span>
       </div>
-      <div className={`${showInfo ? 'show-content' : ''} resume__content text-titleColor dark:text-titleColorDark`}>
-        <div className='resume__date-title'>
-          <h3 className='resume__title text-titleColor dark:text-titleColorDark'>{props.subtitle}</h3>
-          <span className='resume__date text-cs text-titleColor dark:text-titleColorDark'>{props.date}</span>
+      <div
+        className={`${
+          showInfo ? "show-content" : ""
+        } resume__content text-titleColor overflow-y-scroll scrollbar-hide dark:text-titleColorDark`}
+      >
+        <div className="resume__date-title">
+          <h3 className="resume__title text-titleColor dark:text-titleColorDark">
+            {subtitle}
+          </h3>
+          <span className="resume__date text-cs text-titleColor dark:text-titleColorDark">
+            {date}
+          </span>
         </div>
 
-        <p className='resume__desc text-textColor dark:text-textColorDark'>{props.description}</p>
+        {category === "experience" ? (
+          <div className="resume__desc text-textColor dark:text-textColorDark ">
+            {description[0]
+              .trim()
+              .split("●")
+              .filter(Boolean)
+              .map((point, i) => (
+                <p key={i}>● {point.trim()}</p>
+              ))}
+          </div>
+        ) : (
+          <div className="resume__desc text-textColor dark:text-textColorDark">
+            <p>{description}</p>
+          </div>
+        )}
       </div>
     </div>
   );
