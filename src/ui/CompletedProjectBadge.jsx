e86fae85b038373656b3d74c85c0ead4a615ca2c
HarshGupta <harshgupta88911@gmail.com>
2025-11-16 16:17:16 +0530

    update: project count make it dynamic

--- src/ui/CompletedProjectBadge.jsx
+++ src/ui/CompletedProjectBadge.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import AnimatedNumbers from "react-animated-numbers";
+import { project } from "../Data";
 
 function CompletedProjectBadge() {
   return (
     <div className="uppercase absolute bottom-[5%] right-[0] sm:bottom-[12%] sm:right-[6%] lg:bottom-[5%] border-[3px] border-solid border-borderColor bg-containerColor sm:shadow-[5px_5px_rgba(0,0,0,0.2)] dark:sm:shadow-[5px_5px_rgba(0,0,0,0.3)] shadow-[5px_5px_rgba(0,0,0,0.3)] dark:shadow-[5px_5px_rgba(255,255,255,0.3)] pl-[5px] xs:pl-[10px] sm:pl-[20px] w-auto sm:h-[82px] rounded-[82px] flex items-center z-[1]">
       <span className="w-[30%] text-titleColor font-[700] text-center text-[30px] sm:text-[40px]">
         <AnimatedNumbers
-          animateToNumber="7"
+          animateToNumber={project.length}
           locale="en"
           configs={[
             { mass: 1, tension: 220, friction: 100 },
